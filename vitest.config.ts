bb8906cf07f8f8a83a24959263327a723362c437
Felix Scholze <felix@webshaped.de>
2024-01-28 16:12:28 +0100

    build: add vitest, jsdom, prettier

---
+++ vitest.config.ts
@@ -0,0 +1,11 @@
+/// <reference types="vitest" />
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    include: ["tests/unit/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+    exclude: ["tests/unit/__needsFix/*"],
+    globals: true,
+    environment: "jsdom",
+  },
+});
