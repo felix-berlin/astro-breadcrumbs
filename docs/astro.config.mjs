0dd79715efd327ba00d686452f7614050cab4380
Felix Scholze <felix@webshaped.de>
2024-09-07 23:11:05 +0200

    chore: disable links validator

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -56,7 +56,7 @@ export default defineConfig({
           autogenerate: { directory: "guides" },
         },
       ],
-      plugins: [starlightLinksValidator()],
+      // plugins: [starlightLinksValidator()],
       components: {
         Footer: "./src/components/Footer.astro",
       },
