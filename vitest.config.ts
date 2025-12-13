ed9311783606ab67038cf285371a526335f3803b
Felix Scholze <felix@webshaped.de>
2025-12-13 15:10:18 +0100

    test: correct coverage include pattern in vitest configuration

--- vitest.config.ts
+++ vitest.config.ts
@@ -9,7 +9,7 @@ export default getViteConfig(
       environment: "jsdom",
       setupFiles: ["tests/unit/vitest.setup.ts"],
       coverage: {
-        include: ["src/**/**.{js,jsx,ts,tsx,astro}"],
+        include: ["src/**/*.{js,jsx,ts,tsx,astro}"],
         reportsDirectory: "tests/unit/coverage",
       },
     },
